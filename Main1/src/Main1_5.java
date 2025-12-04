f8c3f73dc0e1bb61bc62b7a65cebbec06a049566
en197 <en197@DESKTOP-R2OM4D1>
2025-12-04 13:18:01 +0900

    1章の全部です

---
+++ Main1/src/Main1_5.java
@@ -0,0 +1,13 @@
+public class Main1_5 {
+	//円周率に定数を利用する（エラー）
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		int pie = 5;
+		System.out.println("半径" + pie + "cmのパイの面積は");
+		System.out.println(pie * pie * PI);
+		System.out.println("パイの半径を倍にします");
+		PI = 10;
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * PI);
+	}
+}
