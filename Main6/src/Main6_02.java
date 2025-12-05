373432042e1a22272466c22670ee0bb7c83b0cc0
en197 <en197@DESKTOP-R2OM4D1>
2025-12-05 22:53:50 +0900

    6章の途中まで

---
+++ Main6/src/Main6_02.java
@@ -0,0 +1,10 @@
+public class Main6_02 {
+// CalcLogic.javaに計算処理を追加する
+public class Calclogic {
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+	public static int hiku(int a, int b) {
+		return (a - b);
+	}
+}
