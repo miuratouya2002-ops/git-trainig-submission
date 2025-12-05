8a608d6111c7ece6dd6742bbc9f27048df28f4bc
en197 <en197@DESKTOP-R2OM4D1>
2025-12-05 13:54:42 +0900

    5章のコード5-10まで

---
+++ Main5/src/Main5_01.java
@@ -0,0 +1,6 @@
+public class Main5_01 {
+	// シンプルなメソッドの定義
+	public static void main(String[] args) {
+		System.out.println("湊さん、こんにちは");
+	}
+}
---
+++ Main5/src/Main5_06.java
@@ -0,0 +1,12 @@
+public class Main5_06 {
+// 引数を使わずに値を渡せないのか？（エラー）
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 10;
+		add();
+	}
+	public static void add() {
+		int ans = x + y;
+		System.out.println(x + "+" y + "=" + ans);
+	}
+}
