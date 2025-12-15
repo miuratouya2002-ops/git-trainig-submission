94ffb2288743831a1865573d0a620d1e78e2d9ed
en197 <en197@DESKTOP-R2OM4D1>
2025-12-15 14:13:53 +0900

    14-1から14-7まで

--- Main14/src/Main14_1.java
+++ Main14/src/Main14_1.java
@@ -5,14 +5,14 @@ public class Main14_1 {
 		Main14_1_Empty empty = new Main14_1_Empty();
 		//メモ↓toStringの呼び出し
 		//　　　String sを代入させる
-		String  s = empty.toString();
+		String s = empty.toString();
 		//メモ↓sを表示させる
 		System.out.println(s);
-		
+
 		//メモ　extendsで親クラスを指定しないと、java.lang.objectを親クラスとして継承したとみなされる
 		//　　　→多態性を利用できるようになるから
 		//　　　equals→あるインスタンスと自分自身が同じかを調べる
 		//　　　自分自身の内容の文字列表現を返す
-		
+
 	}
 }
--- Main14/src/Main14_7_Hero.java
+++ Main14/src/Main14_7_Hero.java
@@ -19,3 +19,5 @@ public class Main14_7_Hero {
 	}
 
 }
+
+	
