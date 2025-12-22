60911560c11fb13c267896ee9d84d6a1d9ceaa19
en197 <en197@DESKTOP-R2OM4D1>
2025-12-22 15:45:25 +0900

    5章の全部

--- moattravel/src/main/java/com/example/ServletInitializer.java
+++ moattravel/src/main/java/com/example/ServletInitializer.java
@@ -1,4 +1,4 @@
-package moattravel;
+package com.example;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Demo1Application.class);
+		return application.sources(moattravelApplication.class);
 	}
 
 }
--- moattravel/src/main/java/com/example/moattravelApplication.java
+++ moattravel/src/main/java/com/example/moattravelApplication.java
@@ -1,13 +1,13 @@
-package moattravel;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
+public class moattravelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
+		SpringApplication.run(moattravelApplication.class, args);
 	}
 
 }
