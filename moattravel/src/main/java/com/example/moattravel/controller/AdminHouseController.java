612344a96cbb3508b0d09e152f1c2a6c38614709
en197 <en197@DESKTOP-R2OM4D1>
2025-12-23 18:03:45 +0900

    8章の途中

--- moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -2,6 +2,8 @@ package com.example.moattravel.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,11 +28,15 @@ public class AdminHouseController {
 
 	@GetMapping
 
-	public String index(Model model) {
+	public String index(Model model, Pageable pageable) {
 
 		List<House> houses = houseRepository.findAll();
 
+		Page<House> housePage = houseRepository.findAll(pageable);
+
 		model.addAttribute("houses", houses);
+
+		model.addAttribute("housePage", housePage);
 
 		return "admin/houses/index";
 	}
