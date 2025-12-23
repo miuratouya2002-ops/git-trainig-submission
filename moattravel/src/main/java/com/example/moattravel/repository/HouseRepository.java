9ee073bde8e64bb73c853bd1abb01f3a2fcaa97e
en197 <en197@DESKTOP-R2OM4D1>
2025-12-23 14:03:59 +0900

    moatの7.6まで完了

--- moattravel/src/main/java/com/example/moattravel/repository/HouseRepository.java
+++ moattravel/src/main/java/com/example/moattravel/repository/HouseRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.moattravel.entity.House;
 
-public interface HouseRepository {
-	
-	public interface HouseRepository extends JpaRepository<House, Integer> {
-		
-	}
+public interface HouseRepository extends JpaRepository<House, Integer> {
+
 }
