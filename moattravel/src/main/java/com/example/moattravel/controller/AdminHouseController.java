ab1cce362e7d42be699fd7f0f13e18bd5251abe5
en197 <en197@DESKTOP-R2OM4D1>
2025-12-24 14:10:16 +0900

    9章の途中

--- moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.moattravel.entity.House;
 import com.example.moattravel.repository.HouseRepository;
@@ -29,11 +30,25 @@ public class AdminHouseController {
 	@GetMapping
 
 	public String index(Model model,
-			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable) {
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+			@RequestParam(name = "keyword", required = false) String keyword) {
 
-		Page<House> housePage = houseRepository.findAll(pageable);
+		Page<House> housePage;
+
+		if (keyword != null && !keyword.isEmpty()) {
+
+			housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable);
+		}
+
+		else {
+
+			housePage = houseRepository.findAll(pageable);
+
+		}
 
 		model.addAttribute("housePage", housePage);
+
+		model.addAttribute("keyword", keyword);
 
 		return "admin/houses/index";
 	}
