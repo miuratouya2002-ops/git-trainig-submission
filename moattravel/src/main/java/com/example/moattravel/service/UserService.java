80e49420d79cddd2909a30c727c43af8402974a5
en197 <en197@DESKTOP-R2OM4D1>
2026-01-02 23:10:12 +0900

    25章の全部

--- moattravel/src/main/java/com/example/moattravel/service/UserService.java
+++ moattravel/src/main/java/com/example/moattravel/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.moattravel.entity.Role;
 import com.example.moattravel.entity.User;
 import com.example.moattravel.form.SignupForm;
+import com.example.moattravel.form.UserEditForm;
 import com.example.moattravel.repository.RoleRepository;
 import com.example.moattravel.repository.UserRepository;
 
@@ -54,11 +55,31 @@ public class UserService {
 
 		user.setRole(role);
 
-		user.setEnabled(true);
-
 		user.setEnabled(false);
 
 		return userRepository.save(user);
+
+	}
+
+	@Transactional
+
+	public void update(UserEditForm userEditForm) {
+
+		User user = userRepository.getReferenceById(userEditForm.getId());
+
+		user.setName(userEditForm.getName());
+
+		user.setFurigana(userEditForm.getFurigana());
+
+		user.setPostalCode(userEditForm.getPostalCode());
+
+		user.setAddress(userEditForm.getAddress());
+
+		user.setPhoneNumber(userEditForm.getPhoneNumber());
+
+		user.setEmail(userEditForm.getEmail());
+
+		userRepository.save(user);
 
 	}
 
@@ -89,6 +110,16 @@ public class UserService {
 		user.setEnabled(true);
 
 		userRepository.save(user);
+
+	}
+
+	//メールアドレスが変更されたかどうかをチェックする
+
+	public boolean isEmailChanged(UserEditForm userEditForm) {
+
+		User currentUser = userRepository.getReferenceById(userEditForm.getId());
+
+		return !userEditForm.getEmail().equals(currentUser.getEmail());
 
 	}
 
