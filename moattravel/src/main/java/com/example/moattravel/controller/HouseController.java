90564139fe1e884358c3eac6f099b58a5a6ee172
en197 <en197@DESKTOP-R2OM4D1>
2026-01-05 14:27:48 +0900

    30章の途中

--- moattravel/src/main/java/com/example/moattravel/controller/HouseController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/HouseController.java
@@ -7,6 +7,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -47,8 +48,6 @@ public class HouseController {
 
 		if (keyword != null && !keyword.isEmpty()) {
 
-			housePage = houseRepository.findByNameLikeOrAddressLike("%" + keyword + "%", "%" + keyword + "%", pageable);
-
 			if (order != null && order.equals("priceAsc")) {
 
 				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByPriceAsc("%" + keyword + "%",
@@ -63,8 +62,6 @@ public class HouseController {
 
 		} else if (area != null && !area.isEmpty()) {
 
-			housePage = houseRepository.findByAddressLike("%" + area + "%", pageable);
-
 			if (order != null && order.equals("priceAsc")) {
 
 				housePage = houseRepository.findByAddressLikeOrderByPriceAsc("%" + area + "%", pageable);
@@ -77,8 +74,6 @@ public class HouseController {
 
 		} else if (price != null) {
 
-			housePage = houseRepository.findByPriceLessThanEqual(price, pageable);
-
 			if (order != null && order.equals("priceAsc")) {
 
 				housePage = houseRepository.findByPriceLessThanEqualOrderByPriceAsc(price, pageable);
@@ -90,8 +85,6 @@ public class HouseController {
 			}
 
 		} else {
-
-			housePage = houseRepository.findAll(pageable);
 
 			if (order != null && order.equals("priceAsc")) {
 
@@ -116,6 +109,18 @@ public class HouseController {
 		model.addAttribute("order", order);
 
 		return "houses/index";
+
+	}
+
+	@GetMapping("/{id}")
+
+	public String show(@PathVariable(name = "id") Integer id, Model model) {
+
+		House house = houseRepository.getReferenceById(id);
+
+		model.addAttribute("house", house);
+
+		return "houses/show";
 
 	}
 
