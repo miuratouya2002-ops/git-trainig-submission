e439e96b35929bba0f15390f990dcf8892091a3b
en197 <en197@DESKTOP-R2OM4D1>
2026-01-05 11:04:18 +0900

    28章の全部

--- moattravel/src/main/java/com/example/moattravel/controller/HouseController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/HouseController.java
@@ -35,6 +35,8 @@ public class HouseController {
 
 			@RequestParam(name = "price", required = false) Integer price,
 
+			@RequestParam(name = "order", required = false) String order,
+
 			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
 
 			Model model)
@@ -47,17 +49,57 @@ public class HouseController {
 
 			housePage = houseRepository.findByNameLikeOrAddressLike("%" + keyword + "%", "%" + keyword + "%", pageable);
 
+			if (order != null && order.equals("priceAsc")) {
+
+				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByPriceAsc("%" + keyword + "%",
+						"%" + keyword + "%", pageable);
+			} else {
+
+				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByCreatedAtDesc("%" + keyword + "%",
+						"%" + keyword + "%", pageable);
+
+			}
+
 		} else if (area != null && !area.isEmpty()) {
 
 			housePage = houseRepository.findByAddressLike("%" + area + "%", pageable);
+
+			if (order != null && order.equals("priceAsc")) {
+
+				housePage = houseRepository.findByAddressLikeOrderByPriceAsc("%" + area + "%", pageable);
+
+			} else {
+
+				housePage = houseRepository.findByAddressLikeOrderByCreatedAtDesc("%" + area + "%", pageable);
+			}
 
 		} else if (price != null) {
 
 			housePage = houseRepository.findByPriceLessThanEqual(price, pageable);
 
+			if (order != null && order.equals("priceAsc")) {
+
+				housePage = houseRepository.findByPriceLessThanEqualOrderByPriceAsc(price, pageable);
+
+			} else {
+
+				housePage = houseRepository.findByPriceLessThanEqualOrderByCreatedAtDesc(price, pageable);
+
+			}
+
 		} else {
 
 			housePage = houseRepository.findAll(pageable);
+
+			if (order != null && order.equals("priceAsc")) {
+
+				housePage = houseRepository.findAllByOrderByPriceAsc(pageable);
+
+			} else {
+
+				housePage = houseRepository.findAllByOrderByCreatedAtDesc(pageable);
+
+			}
 
 		}
 
@@ -68,6 +110,8 @@ public class HouseController {
 		model.addAttribute("area", area);
 
 		model.addAttribute("price", price);
+
+		model.addAttribute("order", order);
 
 		return "houses/index";
 
