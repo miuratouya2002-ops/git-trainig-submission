d0e595895da02c1af982949f447b3992e9f20df4
en197 <en197@DESKTOP-R2OM4D1>
2026-01-06 12:54:50 +0900

    33章の全部

---
+++ moattravel/src/main/resources/static/js/flatpickr.js
@@ -0,0 +1,17 @@
+let maxDate = new Date();
+
+maxDate = maxDate.setMonth(maxDate.getMonth() + 3);
+
+
+
+flatpickr('#fromCheckinDateToCheckoutDate', {
+
+	mode: "range",
+
+	locale: 'ja',
+
+	minDate: 'today',
+
+	maxDate: maxDate
+
+});
