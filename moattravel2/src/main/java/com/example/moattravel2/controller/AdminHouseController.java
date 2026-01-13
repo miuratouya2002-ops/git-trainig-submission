b7a53f4eb01bebd7dd46e76216a3d7dd1909343f
miuratouya2002-ops <miuratouya2002@gmail.com>
2026-01-13 15:33:53 +0900

    9章の全部

--- moattravel2/src/main/java/com/example/moattravel2/controller/AdminHouseController.java
+++ moattravel2/src/main/java/com/example/moattravel2/controller/AdminHouseController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.moattravel2.entity.House;
 import com.example.moattravel2.repository.HouseRepository;
@@ -31,11 +32,24 @@ public class AdminHouseController {
 	@GetMapping
 
 	public String index(Model model,
-			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable) {
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+			@RequestParam(name = "keyword", required = false) String keyword) {
 
-		Page<House> housePage = houseRepository.findAll(pageable);
+		Page<House> housePage;
+
+		if (keyword != null && !keyword.isEmpty()) {
+
+			housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable);
+
+		} else {
+
+			housePage = houseRepository.findAll(pageable);
+
+		}
 
 		model.addAttribute("housePage", housePage);
+
+		model.addAttribute("keyword", keyword);
 
 		return "admin/houses/index";
 
--- moattravel2/src/main/java/com/example/moattravel2/repository/HouseRepository.java
+++ moattravel2/src/main/java/com/example/moattravel2/repository/HouseRepository.java
@@ -1,9 +1,13 @@
 package com.example.moattravel2.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.moattravel2.entity.House;
 
 public interface HouseRepository extends JpaRepository<House, Integer> {
+
+	public Page<House> findByNameLike(String keyword, Pageable pageable);
 
 }
