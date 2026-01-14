0216fda6135a7d520ddc6e21c22ef5b4e3a9272c
miuratouya2002-ops <miuratouya2002@gmail.com>
2026-01-14 17:06:04 +0900

    17章の全部

---
+++ moattravel2/src/main/java/com/example/moattravel2/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.moattravel2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}
