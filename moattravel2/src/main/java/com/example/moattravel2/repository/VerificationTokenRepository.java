d76418ed6bb2fb2ff1acfbace983968b0ad339c8
miuratouya2002-ops <miuratouya2002@gmail.com>
2026-01-15 19:01:39 +0900

    23章の全部

---
+++ moattravel2/src/main/java/com/example/moattravel2/repository/VerificationTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.moattravel2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.moattravel2.entity.VerificationToken;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Integer> {
+
+	public VerificationToken findByToken(String token);
+
+}
--- moattravel2/src/main/resources/schema.sql
+++ moattravel2/src/main/resources/schema.sql
@@ -62,4 +62,22 @@ CREATE TABLE IF NOT EXISTS houses (
 			
 			);
 			
+			CREATE TABLE IF NOT EXISTS verification_tokens (
+				
+				id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				
+				user_id INT NOT NULL UNIQUE,
+				
+				token VARCHAR(255) NOT NULL,
+				
+				created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+				
+				 updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+				 
+				 FOREIGN KEY (user_id) REFERENCES users (id)
+				 
+				 );
+				 
+				 
+			
 			
