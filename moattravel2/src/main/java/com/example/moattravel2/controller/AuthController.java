f42ee8abc702bdef3a0139221e6bc4769c30e00b
miuratouya2002-ops <miuratouya2002@gmail.com>
2026-01-15 14:10:49 +0900

    21章の全部

--- moattravel2/src/main/java/com/example/moattravel2/controller/AuthController.java
+++ moattravel2/src/main/java/com/example/moattravel2/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.moattravel2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.moattravel2.form.SignupForm;
 
 @Controller
 
@@ -12,6 +15,16 @@ public class AuthController {
 	public String login() {
 
 		return "auth/login";
+
+	}
+
+	@GetMapping("/signup")
+
+	public String signup(Model model) {
+
+		model.addAttribute("signupForm", new SignupForm());
+
+		return "auth/signup";
 
 	}
 
--- moattravel2/src/main/java/com/example/moattravel2/security/WebSecurityConfig.java
+++ moattravel2/src/main/java/com/example/moattravel2/security/WebSecurityConfig.java
@@ -21,14 +21,17 @@ public class WebSecurityConfig {
 
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/").permitAll() // すべてのユーザーにアクセスを許可するURL 
+		http
+				.authorizeHttpRequests((requests) -> requests
 
-				.requestMatchers("/admin/**").hasRole("ADMIN") // 管理者にのみアクセスを許可するURL
+						.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**")
+						.permitAll() // すべてのユーザーにアクセスを許可するURL 
 
-				.anyRequest().authenticated() // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
+						.requestMatchers("/admin/**").hasRole("ADMIN") // 管理者にのみアクセスを許可するURL
 
-		)
+						.anyRequest().authenticated() // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
+
+				)
 
 				.formLogin((form) -> form
 
