7145c66cb806eac69738d0b707c25a1dd5d3fdb7
miuratouya2002-ops <miuratouya2002@gmail.com>
2026-01-16 18:59:08 +0900

    29章の全部

--- moattravel2/src/main/java/com/example/moattravel2/controller/HomeController.java
+++ moattravel2/src/main/java/com/example/moattravel2/controller/HomeController.java
@@ -1,19 +1,38 @@
 package com.example.moattravel2.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.moattravel2.entity.House;
+import com.example.moattravel2.repository.HouseRepository;
 
 @Controller
 
 public class HomeController {
 
+	private final HouseRepository houseRepository;
+
+	public HomeController(HouseRepository houseRepository) {
+
+		this.houseRepository = houseRepository;
+
+	}
+
 	@GetMapping("/")
 
-	public String index() {
+	public String index(Model model) {
+
+		List<House> newHouses = houseRepository.findTop10ByOrderByCreatedAtDesc();
+
+		model.addAttribute("newHouses", newHouses);
 
 		//return "index";と記述すれば、src/main/resources/templatesフォルダ内にあるindex.htmlが呼び出される
 
 		return "index";
+
 	}
 
 }
