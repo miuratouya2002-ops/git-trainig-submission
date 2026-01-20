66bcc1063463bf63bdcaddf118786d1ee7beb3a6
miuratouya2002-ops <miuratouya2002@gmail.com>
2026-01-20 14:17:05 +0900

    3週の6章

---
+++ moattravel3/src/main/java/com/example/moattravel3/repository/HouseRepository.java
@@ -0,0 +1,9 @@
+package com.example.moattravel3.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.moattravel3.entity.House;
+
+public interface HouseRepository extends JpaRepository<House, Integer> {
+
+}
