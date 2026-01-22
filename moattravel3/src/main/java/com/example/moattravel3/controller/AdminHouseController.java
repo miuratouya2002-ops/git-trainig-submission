35b477ca3ac559e20574e12f2514d036d699c09e
miuratouya2002-ops <miuratouya2002@gmail.com>
2026-01-22 16:04:33 +0900

    14章の全部

--- moattravel3/src/main/java/com/example/moattravel3/controller/AdminHouseController.java
+++ moattravel3/src/main/java/com/example/moattravel3/controller/AdminHouseController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.moattravel3.entity.House;
+import com.example.moattravel3.form.HouseEditForm;
 import com.example.moattravel3.form.HouseRegisterForm;
 import com.example.moattravel3.repository.HouseRepository;
 import com.example.moattravel3.service.HouseService;
@@ -99,6 +100,25 @@ public class AdminHouseController {
 		redirectAttributes.addFlashAttribute("successMessage", "民宿を登録しました。");
 
 		return "redirect:/admin/houses";
+
+	}
+
+	@GetMapping("/{id}/edit")
+
+	public String edit(@PathVariable(name = "id") Integer id, Model model) {
+
+		House house = houseRepository.getReferenceById(id);
+
+		String imageName = house.getImageName();
+
+		HouseEditForm houseEditForm = new HouseEditForm(house.getId(), house.getName(), null, house.getDescription(),
+				house.getPrice(), house.getCapacity(), house.getPostalCode(), house.getAddress(),
+				house.getPhoneNumber());
+
+		model.addAttribute("imageName", imageName);
+		model.addAttribute("houseEditForm", houseEditForm);
+
+		return "admin/houses/edit";
 
 	}
 
