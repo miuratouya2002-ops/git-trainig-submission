22c18061d68b22c5dfc6ddb62e366bca254b3e16
miuratouya2002-ops <miuratouya2002@gmail.com>
2026-01-23 18:19:40 +0900

    23章の全部

---
+++ moattravel3/src/main/java/com/example/moattravel3/event/SignupEventPublisher.java
@@ -0,0 +1,26 @@
+package com.example.moattravel3.event;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+import com.example.moattravel3.entity.User;
+
+@Component
+
+public class SignupEventPublisher {
+
+	private final ApplicationEventPublisher applicationEventPublisher;
+
+	public SignupEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+
+		this.applicationEventPublisher = applicationEventPublisher;
+
+	}
+
+	public void publishSignupEvent(User user, String requestUrl) {
+
+		applicationEventPublisher.publishEvent(new SignupEvent(this, user, requestUrl));
+
+	}
+
+}
