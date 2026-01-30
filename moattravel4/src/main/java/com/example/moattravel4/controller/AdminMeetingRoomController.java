7c1edf478621c399357cafcacf358a0b812a5fbc
miuratouya2002-ops <miuratouya2002@gmail.com>
2026-01-30 13:00:36 +0900

    管理者用の編集ページの作成

--- moattravel4/src/main/java/com/example/moattravel4/controller/AdminMeetingRoomController.java
+++ moattravel4/src/main/java/com/example/moattravel4/controller/AdminMeetingRoomController.java
@@ -6,75 +6,83 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.moattravel4.entity.MeetingRoom;
 import com.example.moattravel4.form.MeetingRoomRegisterForm;
 import com.example.moattravel4.repository.MeetingRoomRepository;
+import com.example.moattravel4.service.MeetingRoomService;
 
 @Controller
 @RequestMapping("/admin/meeting_rooms")
-
 public class AdminMeetingRoomController {
 
 	private final MeetingRoomRepository meetingRoomRepository;
+	private final MeetingRoomService meetingRoomService;
 
-	public AdminMeetingRoomController(MeetingRoomRepository meetingRoomRepository) {
-
+	public AdminMeetingRoomController(MeetingRoomRepository meetingRoomRepository,
+			MeetingRoomService meetingRoomService) {
 		this.meetingRoomRepository = meetingRoomRepository;
-
+		this.meetingRoomService = meetingRoomService;
 	}
 
 	@GetMapping
-
 	public String index(Model model,
 			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
 			@RequestParam(name = "keyword", required = false) String keyword) {
 
-		Page<MeetingRoom> meetingRoomsPage = meetingRoomRepository.findAll(pageable);
+		Page<MeetingRoom> meetingRoomsPage;
 
 		if (keyword != null && !keyword.isEmpty()) {
-
 			meetingRoomsPage = meetingRoomRepository.findByNameLike("%" + keyword + "%", pageable);
-
 		} else {
-
 			meetingRoomsPage = meetingRoomRepository.findAll(pageable);
-
 		}
 
 		model.addAttribute("meetingRoomsPage", meetingRoomsPage);
-
 		model.addAttribute("keyword", keyword);
 
 		return "admin/meeting_rooms/index";
-
 	}
 
 	@GetMapping("/register")
 	public String register(Model model) {
-
 		model.addAttribute("meetingRoomRegisterForm", new MeetingRoomRegisterForm());
-
 		return "admin/meeting_rooms/register";
-
 	}
 
 	@PostMapping("/create")
-	public String create(@ModelAttribute @Validated MeetingRoomRegisterForm meetingRoomRegisterForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-		
-		if (bindingResult, hasErrors()) {
-			
-			return "admin/meeting_rooms/registe";
-			
-			redirectAttributes.addFlashAttribute("successMessage","会議室を登録しました。");
-			
-			return "redirect:/admin/meeting_rooms";
-			
+	public String create(@ModelAttribute @Validated MeetingRoomRegisterForm meetingRoomRegisterForm,
+			BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+		if (bindingResult.hasErrors()) {
+			return "admin/meeting_rooms/register";
 		}
-		
+
+		meetingRoomService.create(meetingRoomRegisterForm);
+
+		redirectAttributes.addFlashAttribute("successMessage", "会議室を登録しました。");
+
+		return "redirect:/admin/meeting_rooms";
+	}
+
+	@GetMapping("/{id}")
+
+	public String show(@PathVariable(name = "id") Integer id, Model model) {
+
+		MeetingRoom meetingRoom = meetingRoomRepository.getReferenceById(id);
+
+		model.addAttribute("meetingRoom", meetingRoom);
+
+		return "admin/meeting_rooms/show";
 	}
 
 }
--- moattravel4/src/main/java/com/example/moattravel4/service/MeetingRoomService.java
+++ moattravel4/src/main/java/com/example/moattravel4/service/MeetingRoomService.java
@@ -1,5 +1,70 @@
 package com.example.moattravel4.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.moattravel4.entity.MeetingRoom;
+import com.example.moattravel4.form.MeetingRoomEditForm;
+import com.example.moattravel4.repository.MeetingRoomRepository;
+
+@Service
 public class MeetingRoomService {
 
+	private final MeetingRoomRepository meetingRoomRepository;
+
+	public MeetingRoomService(MeetingRoomRepository meetingRoomRepository) {
+
+		this.meetingRoomRepository = meetingRoomRepository;
+
+	}
+
+	@Transactional
+
+	public void update(MeetingRoomEditForm meetingRoomEditForm) {
+
+		MeetingRoom meetingRoom = meetingRoomRepository.getReferenceById(meetingRoomEditForm.getId());
+		MultipartFile imageFile = meetingRoomEditForm.getImageFile();
+
+		if (!imageFile.isEmpty()) {
+			String imageName = imageFile.getOriginalFilename();
+			String hashedImageName = generateNewFileName(imageName);
+			Path filePath = Paths.get("src/main/resources/static/storage/" + hashedImageName);
+			copyImageFile(imageFile, filePath);
+			meetingRoom.setImageName(hashedImageName);
+		}
+
+		meetingRoom.setName(meetingRoomEditForm.getName());
+		meetingRoom.setDescription(meetingRoomEditForm.getDescription());
+		meetingRoom.setPrice(meetingRoomEditForm.getPrice());
+		meetingRoom.setCapacity(meetingRoomEditForm.getCapacity());
+		meetingRoom.setPostalCode(meetingRoomEditForm.getPostalCode());
+		meetingRoom.setAddress(meetingRoomEditForm.getAddress());
+		meetingRoom.setPhoneNumber(meetingRoomEditForm.getPhoneNumber());
+
+		meetingRoomRepository.save(meetingRoom);
+	}
+
+	public String generateNewFileName(String fileName) {
+		String[] fileNames = fileName.split("\\.");
+		for (int i = 0; i < fileNames.length - 1; i++) {
+			fileNames[i] = UUID.randomUUID().toString();
+		}
+		String hashedFileName = String.join(".", fileNames);
+		return hashedFileName;
+	}
+
+	public void copyImageFile(MultipartFile imageFile, Path filePath) {
+		try {
+			Files.copy(imageFile.getInputStream(), filePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
